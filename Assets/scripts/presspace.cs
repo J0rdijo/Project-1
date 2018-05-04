df04c513b9ef88071586009e1a1dca86664959bc
Unknown <bartomeu.puig@enti.cat>
2018-05-04 19:43:36 +0200

    0.3.1

--- Assets/scripts/presspace.cs
+++ Assets/scripts/presspace.cs
@@ -11,7 +11,7 @@ public class presspace : MonoBehaviour {
     {
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKey(KeyCode.Space))
                 SceneManager.LoadScene("Level O");
         }
     }
