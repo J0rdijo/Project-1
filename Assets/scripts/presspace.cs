eb105234e9727f9ea5dac01a830c1996bdd14a03
Unknown <bartomeu.puig@enti.cat>
2018-05-05 12:21:36 +0200

    0.3.3
    Correcciones idioma Christian

--- Assets/scripts/presspace.cs
+++ Assets/scripts/presspace.cs
@@ -6,13 +6,10 @@ using UnityEngine.UI;
 
 public class presspace : MonoBehaviour {
 
-
-    public class KeyCodeExample : MonoBehaviour
-    {
         void Update()
         {
-            if (Input.GetKey(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Space))
                 SceneManager.LoadScene("Level O");
         }
     }
-}
+
