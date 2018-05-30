fbbacd5e2c07adcec6001a6f64b1ced8db3ec0dc
Unknown <bartomeu.puig@enti.cat>
2018-05-30 12:49:16 +0200

    0.8.1
    Añadida música en los nieveles y separada de la del menú.

--- Assets/scripts/BackgroundMusic.cs
+++ Assets/scripts/BackgroundMusic.cs
@@ -1,42 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+
 
 public class BackgroundMusic : MonoBehaviour
 {
+    public AudioSource background;
+    public AudioClip menuMusic;
+    public AudioClip gameplayMusic;
+    private Scene actualScene;
+    private Scene lastScene;
 
     // Use this for initialization
     void Start()
     {
-
-    }
-
-    //Play Global
-    private static BackgroundMusic instance = null;
-    public static BackgroundMusic Instance
-    {
-        get { return instance; }
-    }
-
-    void Awake()
-    {
-        if (instance != null && instance != this)
-        {
-            Destroy(this.gameObject);
-            return;
-        }
+        if (Time.realtimeSinceStartup > 3)
+            DestroyObject(gameObject);
         else
-        {
-            instance = this;
-        }
-
-        DontDestroyOnLoad(this.gameObject);
+            DontDestroyOnLoad(gameObject);
     }
-    //Play Gobal End
 
-    // Update is called once per frame
     void Update()
     {
-
+        actualScene = SceneManager.GetActiveScene();
+        if (actualScene.name != lastScene.name)// && actualScene.name != "Menu 1"
+        {
+            if (GameObject.Find("Robot") && gameplayMusic != background.clip)
+            {
+                changeMusic(gameplayMusic);
+            }
+            else if(GameObject.Find("MENUCTRL") && menuMusic != background.clip)
+            {
+                changeMusic(menuMusic);
+            }
+            lastScene = SceneManager.GetActiveScene();
+        }
+    }
+    public void changeMusic(AudioClip music)
+    {
+        background.Stop();
+        background.clip = music;
+        background.Play();
     }
 }
