8ae98b98cb7a7219a06a4bed883d18df8221c179
Christian Hurtado <christian.hurtado@enti.cat>
2018-06-03 14:51:07 +0200

    0.9.3
    Añadidos créditos finales con su respectiva animación.

---
+++ Assets/scripts/pressESC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class pressESC : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("Menu_1");
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+            SceneManager.LoadScene("Menu_1");
+        else if (Input.GetKeyDown(KeyCode.Space))
+            SceneManager.LoadScene("Menu_1");
+    }
+}
+
